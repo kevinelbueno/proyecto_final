3d26c4cac1ab6cfd1c5d5567ab4e52ac359f61f7
CaptainMarraqueta <118637452+CaptainMarraqueta@users.noreply.github.com>
2024-07-10 13:24:25 -0400

    Delete GZero directory

--- GZero/myapp/admin.py
+++
@@ -1,7 +0,0 @@
-# myapp/admin.py
-
-from django.contrib import admin
-from .models import Art, Artista  # Corrige 'Arte' a 'Art'
-
-admin.site.register(Art)
-admin.site.register(Artista)
--- GZero/static/js/menu.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    fetch('../../menu/')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('menu-container').innerHTML = data;
-        });
-});
